2e8f336066518475f89f24952406d4e2d90dcd79
pabloalonso-20 <alonso.pablo20@gmail.com>
2020-11-04 14:25:03 -0600

    added vaporeon to html

--- src/main/java/com/wozu/pokemonDeployment/Controller.java
+++ src/main/java/com/wozu/pokemonDeployment/Controller.java
@@ -1,4 +1,16 @@
 package com.wozu.pokemonDeployment;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pokemon")
 public class Controller {
+
+    @GetMapping("/welcome")
+    public String getWelcome(){
+        return "Welcome Trainer!";
+    }
+
 }
